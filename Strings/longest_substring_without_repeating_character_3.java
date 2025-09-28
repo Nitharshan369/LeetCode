a311e1f8b90b4e57e79d25ad1d37252f6894d020
Nitharshan369 <cknitharshan@gmail.com>
2025-09-28 12:32:13 +0530

    Added Strings folder

---
+++ Strings/longest_substring_without_repeating_character_3.java
@@ -0,0 +1,26 @@
+package Strings;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int l = 0;
+        int res = 0;
+
+        for (int r = 0; r < s.length(); r++) {
+            char c = s.charAt(r);
+
+            if (map.containsKey(c)) {
+                l = Math.max(l, map.get(c) + 1);
+            }
+
+            map.put(c, r);
+
+            res = Math.max(res, r - l + 1);
+        }
+
+        return res;
+    }
+}
