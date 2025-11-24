53bb13166677a4f9ca6a25691cb18368af5f903c
Nitharshan369 <cknitharshan@gmail.com>
2025-11-24 07:26:49 +0530

    Added Problem - 1018

---
+++ Arrays/binary_prefix_divisible_by_five_1018.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<Boolean> prefixesDivBy5(int[] nums) {
+        List<Boolean> res = new ArrayList<>();
+
+        int currentMod = 0;
+
+        for (int bit : nums) {
+            currentMod = ((currentMod << 1) + bit) % 5;
+            res.add(currentMod == 0);
+        }
+
+        return res;
+    }
+}
