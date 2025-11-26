5711ffec4f7abb07af49e7b53425c4a42cf551ba
Nitharshan369 <cknitharshan@gmail.com>
2025-11-26 18:53:18 +0530

    Just did a few changes.

--- Arrays/binary_prefix_divisible_by_five_1018.java
+++ Arrays/binary_prefix_divisible_by_five_1018.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public List<Boolean> prefixesDivBy5(int[] nums) {
         List<Boolean> res = new ArrayList<>();
--- DynamicProgramming/minimum_square_triangulation_of_polygon_1039.java
+++ DynamicProgramming/minimum_square_triangulation_of_polygon_1039.java
@@ -1,5 +1,3 @@
-package DynamicProgramming;
-
 class Solution {
     int[] values;
     int[][] memo;
